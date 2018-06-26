af08ff0bdca2504a9dabdeba5842709df7485ab6
Nevadskiy <nevadskiy@gmail.com>
2018-06-27 02:28:07 +0300

    Fix BullsCows Class

--- src/BullsCows.php
+++ src/BullsCows.php
@@ -2,15 +2,12 @@
 
 class BullsCows
 {
-    protected $combination;
-
     protected $attempts;
 
     protected $comparator;
 
     public function __construct(Combination $combination)
     {
-        $this->combination = $combination;
         $this->attempts = new AttemptsCollection();
         $this->comparator = new Comparator($combination);
     }
